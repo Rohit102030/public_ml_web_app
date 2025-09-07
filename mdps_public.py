9d0c84e9dd961bda9aeb1d2c99452627ac546444
Rohit Kumar <154722581+Rohit102030@users.noreply.github.com>
2025-09-07 15:28:28 +0530

    Add files via upload

---
+++ mdps_public.py
@@ -0,0 +1,12 @@
+/*write a program to calculate the area of a rectangle and circle by overloading the area method.*/
+
+class area{
+void rectangle(){
+ float l=12;
+float b=2;
+float area=l*b;
+System.out.println(area);
+}
+void circle(){
+float pi=3.14;
+float area1= 
